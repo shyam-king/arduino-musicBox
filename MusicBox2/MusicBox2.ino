659ef9510c5c121da95c6f0a54942de3f6756af0
Shyam <m.shyam.tnj@gmail.com>
2019-06-18 12:21:42 +0530

    complete implementation

--- MusicBox2/MusicBox2.ino
+++ MusicBox2/MusicBox2.ino
@@ -4,7 +4,7 @@
     Sub Domain: Embedded Systems
     Functions:  initPins, initTimers, startTimer1, startTimer0, stopTimer1, stopTimer0, main
     Macros: PIN_LOW, PIN_HIGH
-    Global variables: pin_speaker
+    Global variables: pin_speaker, pin_switch[3], freq[8], generating
 
 
     Note: The generated wave is centered at 2.5 V, i.e., 2.5 + 2.5sin(x)
@@ -16,10 +16,10 @@
 #define PIN_LOW(port, pin) PORT##port &= !(1 << pin)
 #define PIN_HIGH(port, pin) PORT##port |= (1 << pin)
 
-#define mod(x) ((x) > 0)? (x) : -(x)
-
 const int pin_speaker = 6; //PORTD ~ OC0A
-volatile byte generating = 0; // flag to check when the generation of wave should stop
+const int pin_switch[] = {0, 1, 2}; //PORTB
+const float freq[] = {0, 880, 987.77f, 523.25, 587.33, 659.25, 698.46, 783.99}; //NOTHING, A5, B5, C5, D5, E5, F5, G5
+uint8_t generating = 0; // flag to check when the generation of wave should stop
 
 /*
     Function name: initPins
@@ -30,6 +30,7 @@ volatile byte generating = 0; // flag to check when the generation of wave shoul
 */
 void initPins() {
     DDRD |= (1 << pin_speaker); //set as output
+    DDRB &= !((1 << pin_switch[0]) | (1 << pin_switch[1]) | (1 << pin_switch[2])); //input
 }
 
 /*
@@ -96,8 +97,33 @@ void stopTimer0() {
 }
 
 /*
+    Function name: setFrequency
+    Input: float fhz : frequency in hertz
+    Output: none
+    Logic: calculate OCR1A and set it
+    start timer 1 
+    Example call: setFrequency(100);
+*/
+void setFrequency(float fhz) {
+    uint16_t T =  10000.0f / fhz * 25; // for 64 prescalar
+    startTimer1(T);
+}
+
+/*
+    Function name: changeFrequency
+    Input: float fhz : frequency in hertz
+    Output: none
+    Logic: calculate OCR1A and set it
+    Example call: changeFrequency(455.5);
+*/
+void changeFrequency(float fhz) {
+    uint16_t T =  10000.0f / fhz * 25; // for 64 prescalar
+    OCR1A = T;
+}
+
+/*
     Function name: generateWave
-    Input: uint16_t fhz : frequency in hertz
+    Input: none
     Output: none
     Logic: a sine wave generated of the given frequency
     T is calculated using the given frequency ( this value is set as OCR1A )
@@ -108,19 +134,46 @@ void stopTimer0() {
     Since op = OCR0A/256 * 5, OCR0A is correspondingly set
     Example call: generateWave(100);
 */
-void generateWave(uint16_t fhz) {
-    uint16_t T =  10000.0f / fhz * 25; // for 64 prescalar
-    Serial.println(T);
-    startTimer1(T);
+void generateWave() {
+    changeFrequency(freq[generating]); //to change OCR1A according to the selected frequency
+    float op =  2.5 + 2.5* sin(TCNT1 * 6.28 / OCR1A); //output voltage
+    OCR0A = op * 51; //51 is approx = 256/5
+}
+
+/*
+    Function name: initGeneration
+    Input: none
+    Output: none
+    Logic: to be called before generateWave()
+    set initial frequency to 0 (no sound) and start timer 0
+    Example call: initGeneration()
+*/
+void initGeneration() {
+    setFrequency(freq[0]);
     OCR0A = 127; //2.5 V (initial voltage)
     startTimer0();
-    generating = 1;
-    while (generating) {
-        float op =  2.5 + 2.5* sin(TCNT1 * 6.28 / T); //output voltage
-        OCR0A = op * 51; //51 is approx = 256/5
-    }
-    stopTimer0();
-    stopTimer1();
+}
+
+/*
+    Function name: readSwitches
+    Input: none
+    Output: uint8_t : the combined value of three switches in portb (0,1,2)
+    if no switch is pressed, produces 0
+    switch 1 alone in "on" produces 1
+    switch 2 alone in "on" produces 2
+    switches (1,2) in "on" produces 3
+    switch 3 in "on" produces 4
+    switches (3,1) in "on" produces 5
+    switches (2,3) in "on" produces 6
+    switches (1,2,3) in "on" produces 7
+    Logic: the first three bits of PINB correspond to the three switches therefore 
+    PINB & 7 results in the output as described above
+    Example call: uint8_t value = readSwitches();
+*/
+uint8_t readSwitches() {
+    uint8_t r = 0;
+    r = (PINB & 7);
+    return r;
 }
 
 int main() {
@@ -128,9 +181,11 @@ int main() {
     initTimers();
     sei(); //enable global interrupts
     Serial.begin(9600);
-    generateWave(600);
-    
+    initGeneration();
     while (1) {
-        //
+        generating = readSwitches();
+        generateWave();
     }
+    stopTimer0();
+    stopTimer1();
 }
